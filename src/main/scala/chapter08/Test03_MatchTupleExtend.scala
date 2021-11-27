6a57e055aee719e73205a37efdfaf2b7694a6e07
fangpli <fangpli@ebay.com>
2021-11-27 16:25:27 +0800

    function program

--- src/main/scala/chapter08/Test03_MatchTupleExtend.scala
+++ src/main/scala/chapter08/Test03_MatchTupleExtend.scala
@@ -18,19 +18,19 @@ object Test03_MatchTupleExtend {
     println("+++++++ " * 10)
     val list: List[(String, Int)] = List(("a", 12), ("b", 35), ("c", 27), ("a", 50))
     // 2.1
-    for (elem <- list){
+    for (elem <- list) {
       println(elem._1 + " " + elem._2)
     }
     // 2.2
-    for ((word, count) <- list){
+    for ((word, count) <- list) {
       println(word + " " + count)
     }
     // 2.3 only access _1
-    for ((word, _) <- list){
+    for ((word, _) <- list) {
       println(word)
     }
     // 2.4  only match _1 == "a"
-    for (("a", count) <- list){
+    for (("a", count) <- list) {
       println(count)
     }
   }
