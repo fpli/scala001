ec9e5b541782ae15c7c344f2a26db1bea985efd1
fangpli <fangpli@ebay.com>
2021-12-27 10:18:54 +0800

    function program

--- src/main/java/org/ebay/TestDynamicBind.java
+++ src/main/java/org/ebay/TestDynamicBind.java
@@ -1,33 +1,52 @@
-package org.ebay;
+package org.ebay.scala.org.ebay;
 
 public class TestDynamicBind {
 
     public static void main(String[] args) {
-        Worker worker = new Worker();
+        Worker worker = new Worker("mary");
         System.out.println(worker.name);
         worker.hello();
         worker.hi();
 
         System.out.println("--------------------");
         //
-        Person person = new Worker();
+        Person person = new Worker("alice");
         System.out.println(person.name);// static bind
         person.hello();
+
+        Person p = new Person("john");
+        System.out.println(p);
     }
 
 }
 class Person {
 
-    String name = "person";
+    private Job job = new Job();
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    String name = null;
 
     public void hello() {
         System.out.println("hello person");
     }
 }
 
+class Job {
+    public Job() {
+        System.out.println("----");
+    }
+}
+
 class Worker extends Person{
 
     String name = "worker";
+
+    public Worker(String name) {
+        super(name);
+    }
 
     public void hello() {
         System.out.println("hello worker");
