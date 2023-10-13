e868124cffaf2c52f6b17096d39592b28f26fd9b
fangpli <fangpli@ebay.com>
2023-10-13 14:31:47 +0800

    case variable@class

--- src/main/scala/chapter08/Test05_MatchCaseClass.scala
+++ src/main/scala/chapter08/Test05_MatchCaseClass.scala
@@ -35,8 +35,8 @@ object Test05_MatchCaseClass {
 
     def showNotification(notification: Notification): String = {
       notification match {
-        case Email(sender, title, _) =>
-          s"You got an email from $sender with title: $title"
+        case email@Email(sender, title, _) =>
+          s"You got an email from $sender with title: ${email.title}, tilte: $title"
         case SMS(number, message) =>
           s"You got an SMS from $number! Message: $message"
         case VoiceRecording(name, link) =>
@@ -54,9 +54,9 @@ object Test05_MatchCaseClass {
     println("--------------pattern guards--------")
     def showImportantNotification(notification: Notification, importantPeopleInfo: Seq[String]): String = {
       notification match {
-        case Email(sender, _, _) if importantPeopleInfo.contains(sender) =>
+        case email@Email(sender, _, _) if importantPeopleInfo.contains(sender) =>
           "You got an email from special someone!"
-        case SMS(number, _) if importantPeopleInfo.contains(number) =>
+        case sms@SMS(number, _) if importantPeopleInfo.contains(number) =>
           "You got an SMS from special someone!"
         case other =>
           showNotification(other) // nothing special, delegate to our original showNotification function
